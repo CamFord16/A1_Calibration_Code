9523641e34820915910d170874f2982dbd943e17
Konstantinos Pantelios <konstantinospantelios@yahoo.com>
2021-03-02 16:22:10 +0100

    Remove Liang's comments.

--- Calibration/camera_calibration_method.cpp
+++ Calibration/camera_calibration_method.cpp
@@ -53,51 +53,31 @@ bool CameraCalibration::calibration(
         vec3& t)
 {
     std::cout << std::endl;
-    std::cout << "TODO: I am going to implement the calibration() function in the following file:" << std::endl
+    std::cout << "I am going to implement the calibration() function in the following file:" << std::endl
               << "\t" << __FILE__ << std::endl;
-    std::cout << "TODO: After implementing the calibration() function, I will disable all unrelated output ...\n\n";
 
-    // TODO: check if input is valid (e.g., number of correspondences >= 6, sizes of 2D/3D points must match)
-    //if (points_3d.size() < 6 || points_2d.size() < 6) std::cout << "Input file contains invalid number of 2D/3D points.\n"; return false;
-    //for (int i = 0; i < points_3d.size(); i++) if (points_3d[i].length()!=3) std::cout << "Input file contains invalid size of 3D points.\n"; return false;
-    //for (int i = 0; i < points_2d.size(); i++) if (points_3d[i].length()!=2) std::cout << "Input file contains invalid size of 3D points.\n"; return false;
+    // check if input is valid (e.g., number of correspondences >= 6, sizes of 2D/3D points must match)
+    if (points_3d.size() < 6 || points_2d.size() < 6) {std::cout << "Input file contains invalid number (<6) of 2D/3D points.\n"; return false;}
+    for (int i = 0; i < points_3d.size(); i++){
+        if (points_3d[i].size() !=3){
+            std::cout << points_3d.size() << "Input file contains invalid size of 3D points.\n";
+            return false;}
+    }
+    for (int i = 0; i < points_2d.size(); i++) {
+        if (points_2d[i].size() != 2) {
+            std::cout << "Input file contains invalid size of 2D points.\n";
+            return false;
+        }
+    }
+    if (points_3d.size() != points_2d.size()) {
+        std::cout << "Input file must contain (pairs) the same number of points for 2D and 3D.\n";
+        return false;
+    }
+    // check if input is valid (e.g., number of correspondences >= 6, sizes of 2D/3D points must match)
 
-
-
-
-    // TODO: solve for M (the whole projection matrix, i.e., M = K * [R, t]) using SVD decomposition.
-    //   Optional: you can check if your M is correct by applying M on the 3D points. If correct, the projected point
-    //             should be very close to your input images points.
-
-    // TODO: extract intrinsic parameters from M.
-
-    // TODO: extract extrinsic parameters from M.
-
-    // TODO: uncomment the line below to return true when testing your algorithm and in you final submission.
-    //return false;
-
-
-
-    // TODO: The following code is just an example showing you SVD decomposition, matrix inversion, and some related.
-    // TODO: Delete the code below (or change "#if 1" in the first line to "#if 0") in you final submission.
 #if 1
-    std::cout << "[Liangliang:] Camera calibration requires computing the SVD and inverse of matrices.\n"
-                 "\tIn this assignment, I provide you with a Matrix data structure for storing matrices of arbitrary\n"
-                 "\tsizes (see matrix.h). I also wrote the example code to show you how to:\n"
-                 "\t\t- use the dynamic 1D array data structure 'std::vector' from the standard C++ library;\n"
-                 "\t\t  The points (both 3D and 2D) are stored in such arrays;\n"
-                 "\t\t- use the template matrix class (which can have an arbitrary size);\n"
-                 "\t\t- compute the SVD of a matrix;\n"
-                 "\t\t- compute the inverse of a matrix;\n"
-                 "\t\t- compute the transpose of a matrix.\n"
-                 "\tThe following are just the output of these examples. You should delete ALL unrelated code and\n"
-                 "\tavoid unnecessary output in you final submission.\n\n";
-
-
-    // TODO: construct the P matrix (so P * m = 0).
-
+    //construct the P matrix (so P * m = 0).
     std::vector<double> array = {};
-
             for (int i = 0; i < points_3d.size(); i++) {
                 for (int j = 0; j < 2; j++) {
                     if (j == 0) {
@@ -120,55 +100,55 @@ bool CameraCalibration::calibration(
                             array.push_back(-points_2d[i].y * (points_3d[i].y));
                             array.push_back(-points_2d[i].y * (points_3d[i].z));
                             array.push_back(-points_2d[i].y);
-
                     }
                 }
             }
 
     const int r = 18, c = 12;
-    Matrix<double> P(r, c, array.data());    // 'array.data()' returns a pointer to the array.
-    std::cout << "P: \n" << P << std::endl;
+    Matrix<double> P(r, c, array.data());
+    //std::cout << "P: \n" << P << std::endl;
 
-
-
-
-
-
+    // Compute the SVD decomposition of A
     Matrix<double> U(r, r, 0.0);   // initialized with 0s
     Matrix<double> S(r, c, 0.0);   // initialized with 0s
     Matrix<double> V(c, c, 0.0);   // initialized with 0s
 
 
-    // Compute the SVD decomposition of A
     svd_decompose(P, U, S, V);
+    // Compute the SVD decomposition of A
 
-    std::cout << "V: \n" << V << std::endl;
 
-    //std::vector<double> m;
+    //get m matrix
     Matrix<double> m(12,1, 0.0);
     for (int i = 0; i <12; i++) {
         m[i][0] = V.get_column(11)[i];
     }
-    std::cout << m << std::endl;
+    //get m matrix
+
+    //get M matrix
     mat34 M(1.0f);
     M.set_row(0, vec4(m[0][0],m[0][1],m[0][2],m[0][3]));
     M.set_row(1, vec4(m[0][4],m[0][5],m[0][6],m[0][7]));
     M.set_row(2, vec4(m[0][8],m[0][9],m[0][10],m[0][11]));
-    std::cout << M <<std::endl;
+    //std::cout << M <<std::endl;
+    //get m matrix
 
+    //set A matrix from M
     mat3 A;
     A.set_row(0,vec3(M.row(0)[0],M.row(0)[1],M.row(0)[2]));
     A.set_row(1,vec3(M.row(1)[0],M.row(1)[1],M.row(1)[2]));
     A.set_row(2,vec3(M.row(2)[0],M.row(2)[1],M.row(2)[2]));
+    //set A matrix from M
 
 
+    //set b matrix from M
     Mat<3,1,float> b;
     b[0] = M.col(3)[0];
     b[1] = M.col(3)[1];
     b[2] = M.col(3)[2];
+    //set b matrix from M
 
-    std::cout << A <<std::endl << b << std::endl;
-
+    //extract intrinsic parameters from M =[A,b]
     double rho = 1/sqrt(A.row(2).x * A.row(2).x + A.row(2).y*A.row(2).y+A.row(2).z*A.row(2).z);
     cx= rho*rho*(dot(A.row(0),(A.row(2))));
     cy = rho*rho*(dot(A.row(1),(A.row(2))));
@@ -181,8 +161,10 @@ bool CameraCalibration::calibration(
     fx = rho*rho*mag13*sin(theta);
     fy = rho*rho*mag23*sin(theta);
 
-    std::cout << "The intrinsics:\nρ: "<< rho << "\nc_x: "<< cx << "\nc_y: "<<cx<<"\nθ: " << theta << "\nf_x: "<< fx << "\nf_y: " << fy << std::endl;
+    //extract intrinsic parameters from M =[A,b]
 
+
+    //extract extrinsic parameters from M =[A,b]
     vec3 r1 = a2a3 / mag23;
     vec3 r3 = rho*A.row(2);
     vec3 r2 = cross(r3,r1);
@@ -190,21 +172,20 @@ bool CameraCalibration::calibration(
     R.set_row(1, r2);
     R.set_row(2, r3);
 
-
     mat3 K;
     skew = -fx*1/tan(theta);
     K.set_row (0, vec3(fx, skew, cx));
     K.set_row(1, vec3(0, fy/sin(theta),cy));
     K.set_row(2, vec3(0, 0, 1));
 
-
     mat3 InvK = inverse(K);
     auto T =  InvK*rho*b;
     t = T.col(0);
-    std::cout << "The extrinsic:\n Rotation Matrix: \n"<< R << "\nTranslation Matrix: \n"<< t << std::endl;
 
+    //extract extrinsic parameters from M =[A,b]
+    std::cout << "The intrinsic parameters:\n" << "\ncx: "<< cx << "\ncy: "<<cy<< "\nfx: "<< fx << "\nfy: " << fy <<"\nskew: "<< skew << "\n"<<std::endl;
+    std::cout << "The extrinsic parameters:\n" <<"\nRotation Matrix: \n"<< R << "\nTranslation Matrix: \n"<< t << std::endl;
     return true;
-    // TODO: delete the above code in you final submission (which are just examples).
 #endif
 }
 
